24c155b9ae67d39d4c481d6fa3fbde638873cd6f
panagiotis kotsorgios <161494302+PanagiotisKotsorgios@users.noreply.github.com>
2025-07-07 11:26:34 +0300

    Update 404.php

--- src/main/404.php
+++ src/main/404.php
@@ -1,51 +1,7 @@
-<?php
-/*
+<?php/*
 ===============================================================================
   GPLMS (General Purpose Library Management System)
 ===============================================================================
-  Project Repository : https://github.com/PanagiotisKotsorgios/gplms
-  License            : MIT Licence
-  Copyright          : (c) 2025 Panagiotis Kotsorgios, Fotis Markantonatos & Contributors
-  Website            : [+]
-
-  Description:
-    GPLMS is a free and open-source Library Management System for schools,
-    universities, and public libraries. It is built using PHP, HTML, JavaScript,
-    and MySQL, and is designed to be modular, extensible, and easy to deploy.
-
-  Creates At:
-    - SAEK MESOLOGHIOY [MESOLOGHI] [GREECE]
-    - WEBSITE: [https://www.saekmesol.gr/]
-            
-  This File:
-    - [404.php]
-    - Purpose: [A Custom made 404 response template]
-
-  Documentation:
-    - Setup Guide         : https://github.com/PanagiotisKotsorgios/gplms/blob/main/README.md
-    - User Guide          : https://github.com/PanagiotisKotsorgios/gplms/blob/main/docs/README.md
-
-  Contributing:
-    - Please see the contributing guide at 
-      https://github.com/PanagiotisKotsorgios/gplms/blob/main/CONTRIBUTION.md
-
-  License Notice:
-
-    This project was originally created by students and independent open-source developers,
-    not by a professional company. It is made for the community, by the community, in the
-    spirit of open source and collective learning. Contributions, use, and sharing are
-    greatelly encouraged!
-
-    This program is free software: you can use, copy, modify, merge, publish,
-    distribute, sublicense, and/or sell copies of it under the terms of the MIT License.
-    See https://opensource.org/licenses/MIT for details.
-
-    WARNING: This software is provided as-is, without any warranty of any kind.
-    That means there are no guarantees, either express or implied, including but not limited to
-    merchantability, fitness for a particular purpose, or non-infringement.
-    The authors and contributors are not responsible for any issues, damages, or losses
-    that may arise from using, modifying, or distributing this software. 
-    You use this project entirely at your own risk.
 
     Thank you for using our software 😁💖
 ===============================================================================
